8c3a5e09594d2219b9216a56230d557259b48852
Matheus Barbosa <mattbarb93@gmail.com>
2020-09-07 00:45:33 -0400

    small commit

--- includes/dgios-customizer-settings-fields.php
+++ includes/dgios-customizer-settings-fields.php
@@ -43,18 +43,18 @@ function dgios_customizer_settings() {
     'dgios_customizer'
   );
 
-  // add_settings_field(
-  //   // Unique identifier for field
-  //   'dgios_customizer_settings_custom_text',
-  //   // Field Title
-  //   __( 'Image Upload', 'dgios_customizer'),
-  //   // Callback for field markup
-  //   'navigation_gallery_media_upload',
-  //   // Page to go on
-  //   'dgios_customizer',
-  //   // Section to go in
-  //   'dgios_customizer_settings_section'
-  // );
+  add_settings_field(
+    // Unique identifier for field
+    'dgios_customizer_settings_custom_text',
+    // Field Title
+    __( 'Image Upload', 'dgios_customizer'),
+    // Callback for field markup
+    'navigation_gallery_media_upload',
+    // Page to go on
+    'dgios_customizer',
+    // Section to go in
+    'nav_gallery_settings_section'
+  );
 
 
   register_setting(
@@ -104,5 +104,42 @@ function navigation_gallery_media_upload() {
   <?php submit_button("Upload") ?>
 </form>';
 
+}
+
+add_action('admin_menu', 'test_plugin_setup_menu');
+ 
+function test_plugin_setup_menu(){
+    add_menu_page( 'Test Plugin Page', 'Test Plugin', 'manage_options', 'test-plugin', 'test_init' );
+}
+ 
+function test_init(){
+    test_handle_post();
+?>
+    <h1>Hello World!</h1>
+    <h2>Upload a File</h2>
+    <!-- Form to handle the upload - The enctype value here is very important -->
+    <form  method="post" enctype="multipart/form-data">
+        <input type='file' id='test_upload_pdf' name='test_upload_pdf'></input>
+        <?php submit_button('Upload') ?>
+    </form>
+<?php
+}
+ 
+function test_handle_post(){
+    // First check if the file appears on the _FILES array
+    if(isset($_FILES['test_upload_pdf'])){
+        $pdf = $_FILES['test_upload_pdf'];
+ 
+        // Use the wordpress function to upload
+        // test_upload_pdf corresponds to the position in the $_FILES array
+        // 0 means the content is not associated with any other posts
+        $uploaded=media_handle_upload('test_upload_pdf', 0);
+        // Error checking using WP functions
+        if(is_wp_error($uploaded)){
+            echo "Error uploading file: " . $uploaded->get_error_message();
+        }else{
+            echo "File upload successful!";
+        }
+    }
 }
 
